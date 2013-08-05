819a363bb820252d3e08a6e5a3b9262cf79e9254
alexgro <you@example.com>
2013-08-06 01:32:04 +0400

    test

---
+++ myobj/appscms/config/user/main.php
@@ -0,0 +1,6 @@
+<?php
+$main_user = array(
+	'homeDirStoreFile' => 'media/upload/storefile',
+	'ClassesFilesStorageProc' => array_merge($main['ClassesFilesStorageProc'],array('classFilesStorageTest' => 'Test',)),
+'countelements' => 10,
+);
