882b134fde619da4f55881231f42f569f688b4cb
alexgro <you@example.com>
2013-09-08 01:39:48 +0400

    normal menu, none alias

--- myobj/appscms/config/user/menu_modul_catalog.php
+++ myobj/appscms/config/user/menu_modul_catalog.php
@@ -2,10 +2,10 @@
 $menu_user = array(
 	'catalog'=>array('label'=>'catalog',
 		'items'=>array(
-			'section'=>array('label'=>'section', 'url'=>array('admin/objects/models/model_catalog')),
-			'sectionoption'=>array('label'=>'section option', 'url'=>array('admin/objects/models/model_catalog_option'),
+			'section'=>array('label'=>'section', 'url'=>array('admin/objects/models/ccatalogCategory')),
+			'sectionoption'=>array('label'=>'section option', 'url'=>array('admin/objects/models/ccatalogOption'),
 				'items'=>array(
-					'optionparams'=>array('label'=>'option params', 'url'=>array('admin/objects/models/model_catalog_option_params'))
+					'optionparams'=>array('label'=>'option params', 'url'=>array('admin/objects/models/ccatalogOptionParam'))
 				),
 			),
 			'graphic_sale'=>array('label'=>'graphic sales', 'url'=>array('admin/objects/ui/graphic_sale')),
--- myobj/appscms/config/user/models_modul_catalog.php
+++ myobj/appscms/config/user/models_modul_catalog.php
@@ -3,26 +3,21 @@ $models = array(
 	//STORE
 	'ccatalogCategory' => array(
 		'namemodel' => 'ccatalogCategory',
-		'relation' => array('options'=>array('options','ccatalogOption')),
+		'relation' => array('options'=>array('ccatalogOption','categories')),
 		'releate_name' => array('ccatalogCategory'=>'categories'),
 	),
 	'ccatalogOption' => array(
 		'namemodel' => 'ccatalogOption',
-		'relation' => array('params'=>'ccatalogOptionParam','categories'=>'ccatalogCategory'),
+		'relation' => array('params'=>array('ccatalogOptionParam','option'),'categories'=>array('ccatalogCategory','options')),
 		'releate_name' => array('CTLoption'=>'option','ccatalogOption'=>'options'),
 	),
 	'ccatalogOptionParam' => array(
 		'namemodel' => 'ccatalogOptionParam',
-		'relation' => array('option'=>'CTLoption'),
+		'relation' => array('option'=>array('ccatalogOption','params')),
 		'releate_name' => array('ccatalogOptionParam'=>'params'),
 	),
 	'graphic_sale' => array(
 		'controller' => 'admin/dep_store/graphic_sale.php',
 	),
-	'CTLoption' => 'ccatalogOption',
-
-	'model_catalog' => 'ccatalogCategory',
-	'model_catalog_option' => 'ccatalogOption',
-	'model_catalog_option_params' => 'ccatalogOptionParam',
 );
 Yii::app()->params['api_conf_models'] =  array_merge(Yii::app()->params['api_conf_models'],$models);
